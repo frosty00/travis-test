1bc67944479195950f83a4f1e4ffb5204c84e81d
Carlo Revelli <carlo.revelli@berkeley.edu>
2020-04-16 13:24:04 -0400

    k

--- script.sh
+++ script.sh
@@ -4,5 +4,5 @@ echo "lolmate" >> trash
 git add trash
 git commit -m 'travis commit'
 git remote remove origin
-git remote add origin "https://${GITHUB_TOKEN}@github.com/ccxt/ccxt.git"
+git remote add origin "https://${GITHUB_TOKEN}@github.com/frosty00/travis-test.git"
 git push origin master
