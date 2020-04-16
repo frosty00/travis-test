8d1f4857c405098dcd3543d2b10e382a66c898ad
Carlo Revelli <carlo.revelli@berkeley.edu>
2020-04-16 13:18:04 -0400

    test

--- script.sh
+++ script.sh
@@ -1,1 +1,8 @@
-echo "hello world"
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+echo "lolmate" >> trash
+git add trash
+git commit -m 'travis commit'
+git remote remove origin
+git remote add origin "https://${GITHUB_TOKEN}@github.com/ccxt/ccxt.git"
+git push origin master
