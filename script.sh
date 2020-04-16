8203df94b9ce6e0e8070b0553d95ab23967f483a
Carlo Revelli <carlo.revelli@berkeley.edu>
2020-04-16 13:29:37 -0400

    save

--- script.sh
+++ script.sh
@@ -1,8 +1,9 @@
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
+git checkout origin/master
 echo "lolmate" >> trash
 git add trash
 git commit -m 'travis commit'
 git remote remove origin
 git remote add origin "https://${GITHUB_TOKEN}@github.com/frosty00/travis-test.git"
-git push origin master
+git push -v origin master
